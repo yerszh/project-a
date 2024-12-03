3a06cc2acff1ed7cb40d8ec58b2575ce46ea9db7
yerszh <yers.zh@gmail.com>
2024-12-04 01:14:02 +0500

    result page

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+"use client";
 
-const ResultPage: React.FC = async () => {
-  return <></>;
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+const ResultPage: React.FC = () => {
+  return (
+    <>
+      <div className="w-full flex justify-between">
+        <Link href="/" className="flex gap-2">
+          <Image
+            src="/icons/refresh-button.svg"
+            alt={"refresh-button.svg"}
+            height={24}
+            width={24}
+          />
+          Пройти заново
+        </Link>
+
+        <Link href="/">
+          <Image
+            src="/icons/close-button.svg"
+            alt={"close-button"}
+            height={24}
+            width={24}
+          />
+        </Link>
+      </div>
+    </>
+  );
 };
 
 export default ResultPage;
