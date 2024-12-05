42529eb5641ef7110457bf0cc14ce65a863bdfbc
yzhumyro <yers.zhumyrov@gmail.com>
2024-12-05 11:25:25 +0500

    chat ui

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -36,7 +36,7 @@ const ChatPage: React.FC = () => {
         </Link>
 
         <div className="flex gap-1 items-center">
-          <h1 className="text-sm text-[#171A1D] font-semibold">
+          <h1 className="text-sm text-[#171A1D] font-bold">
             Smart Bolashaq AI chat
           </h1>
         </div>
@@ -49,23 +49,30 @@ const ChatPage: React.FC = () => {
         />
       </div>
 
-      <div className="max-w-[236px]">
-        <h1 className="text-[12px] text-[#171A1D] font-semibold leading-8 text-center">
-          Привет, я AI Chat! Я тут чтобы помочь тебе узнать информацию о
+      <div className="mt-56 mx-5">
+        <h2 className="text-[#171A1D] text-sm font-normal leading-4 text-center">
+          Привет, я <span className="font-semibold">AI Chat!</span> Я тут чтобы помочь тебе узнать информацию о
           различных профессиях, вузах, грандах, стипендиях, и многое другое. Для
           начала работы выбери профессию.
-        </h1>
-        <p className="mt-6 text-[14px] text-[#171A1D] font-normal leading-4 text-center">
+        </h2>
+        <p className="mt-6 mx-14 text-[#757575] text-xs font-normal leading-3 text-center">
           Диалог в чате ведется на основе выбранной вами профессии
         </p>
       </div>
       <div>
         <Dialog>
-          <DialogTrigger>Подробнее..</DialogTrigger>
+          <DialogTrigger className="mt-2.5 text-[#171A1D] text-xs font-medium leading-3 text-center">Подробнее..</DialogTrigger>
           <DialogContent>
             <DialogHeader>
-              <DialogTitle>Помощь</DialogTitle>
-              <DialogDescription>
+              <DialogTitle className="flex flex-row gap-1.5 text-[#171A1D] text-base	font-semibold leading-4"> 
+                <Image
+            src="/icons/help-icon.svg"
+            alt={"help-icon"}
+            height={16}
+            width={16}
+          />
+          Помощь</DialogTitle>
+              <DialogDescription className="text-[#171A1D] text-[13px] font-medium leading-5 !mt-4">
                 Чат фокусируется на профессии, которую вы выбрали. Чтобы начать
                 обсуждение другой профессии необходимо сначала сменить выбор.
               </DialogDescription>
@@ -76,7 +83,15 @@ const ChatPage: React.FC = () => {
 
       <div>
         <Sheet>
-          <SheetTrigger>Выбрать профессию</SheetTrigger>
+          <SheetTrigger className=" mt-11 flex flex-row gap-1.5 text-[#171A1D] text-[13px] font-medium leading-4 text-center border rounded-3xl p-3"> 
+          <Image
+            src="/icons/message-search.svg"
+            alt={"message-search"}
+            height={16}
+            width={16}
+          />
+            Выбрать профессию</SheetTrigger>
+
           <SheetContent side={"bottom"} className="max-h-[80vh]">
             <SheetHeader>
               <SheetTitle>Профессии</SheetTitle>
@@ -86,8 +101,8 @@ const ChatPage: React.FC = () => {
           </SheetContent>
         </Sheet>
       </div>
-      <div className="bg-[#F5F5F5] flex w-full items-center rounded-full p-1.5">
-        <Textarea placeholder="Сообщение" />
+      <div className="bg-[#F5F5F5] flex w-full items-center rounded-full p-1.5 mt-52">
+        <Textarea className="ml-3" placeholder="Сообщение" />
         <Button type="submit" variant={"icon"} size={"icon"}>
           <Image
             src="/icons/send-message-button.svg"
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <div className="bg-white max-480:bg-[#171A1D] max-480:h-screen max-480:flex max-480:items-center">
-          <main className="max-w-[480px] w-full mx-auto flex flex-col items-center bg-white max-480:rounded-3xl">
+        <div className="bg-white max-480:bg-[#171A1D] max-480:flex max-480:items-center">
+          <main className="max-w-[480px] w-full mx-auto flex flex-col items-center bg-white max-480:rounded-3xl p-4">
             {children}
           </main>
         </div>
