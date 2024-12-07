41dccc0c1843b6d82f7b4d82307ec115fa311e3e
yzhumyro <yers.zhumyrov@gmail.com>
2024-12-07 22:44:51 +0500

    sing-in page

--- src/app/(routes)/auth/sing-in/page.tsx
+++ src/app/(routes)/auth/sing-in/page.tsx
@@ -2,8 +2,9 @@ import { redirect } from "next/navigation";
 import { SingInPage } from "./SingInPage";
 
 const SingIn: React.FC = async () => {
+  // TODO
   const isAuthenticated = false;
-
+// const isAuthenticated = await checkIsAuthenticated();
   if (isAuthenticated) {
     redirect("/");
   } else {
