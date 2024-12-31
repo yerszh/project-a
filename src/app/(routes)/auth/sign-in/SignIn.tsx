a8395ed80822c9952f444a668d8381ce35fef106
yerszh <yers.zh@gmail.com>
2024-12-31 14:10:35 +0500

    sing in style

--- src/app/(routes)/auth/sign-in/SignIn.tsx
+++ src/app/(routes)/auth/sign-in/SignIn.tsx
@@ -24,21 +24,23 @@ export const SignIn: React.FC = () => {
 
   return (
     <div className="p-4 w-full flex flex-col items-center">
-      <h1 className="text-sm text-[#171A1D] font-semibold">
-        AI Профориентатор
-      </h1>
+      <div className="flex gap-1 items-center mt-24">
+        <Image src="/icons/logo.svg" alt={"logo"} height={24} width={24} />
+        <h1 className="text-sm text-[#171A1D] font-semibold ">
+          AI Профориентатор
+        </h1>
+      </div>
 
       <h2 className="text-2xl font-semibold mt-20">Авторизация</h2>
-      <p className="text-sm">Пожалуйста введите данные для входа.</p>
+      <p className="text-sm text-[#A5AAB3]">
+        Пожалуйста введите данные для входа.
+      </p>
 
-      <form
-        className="w-full flex flex-col items-center mt-12"
-        onSubmit={handleSubmit}
-      >
-        <label className="">Email адрес</label>
+      <form className="w-full flex flex-col  mt-12" onSubmit={handleSubmit}>
+        <label className="text-xs text-[#6F7581]">Email адрес</label>
         <Input
           className={
-            "mt-1.5 flex w-full !border-transparent bg-[#F1F4F8]  text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50"
+            "mt-1.5 h-14 px-4 flex w-full !border-transparent bg-[#F1F4F8]  text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50"
           }
           placeholder="Введите свой email"
           type="email"
@@ -59,10 +61,10 @@ export const SignIn: React.FC = () => {
         </Button>
       </form>
 
-      <div className="mt-8">
-        <div className="line"></div>
-        <span className="or">или</span>
-        <div className="line"></div>
+      <div className="flex items-center w-full mt-8">
+        <div className="flex-grow border-t border-[#E3E6EB]"></div>
+        <span className="px-4 text-sm text-[#A5AAB3]">или</span>
+        <div className="flex-grow border-t border-[#E3E6EB]"></div>
       </div>
 
       <Button
