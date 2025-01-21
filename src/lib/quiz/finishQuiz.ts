e1011a3a0077b2b48189ba20be1c6c6e2fd36a7a
yzhumyro <yers.zhumyrov@gmail.com>
2025-01-21 16:56:34 +0500

    finishQuiz result google script

--- src/lib/quiz/finishQuiz.ts
+++ src/lib/quiz/finishQuiz.ts
@@ -3,6 +3,8 @@
 import { prisma } from "@/lib/prisma";
 import { setResultRIASEC } from "../result/setResultRIASEC";
 import { setUserProfessions } from "../result/setUserProfessions";
+import { getLastUserResult } from "../result/getLastUserResult";
+import { auth } from "../auth/authConfig";
 
 export const finishQuiz = async (quizId: string) => {
   try {
@@ -22,6 +24,40 @@ export const finishQuiz = async (quizId: string) => {
         await setUserProfessions(result);
       }
     }
+
+    const session = await auth();
+    const userEmail = session?.user?.email
+    const userResult = await getLastUserResult();
+
+    const json = {
+      email: userEmail,
+      r: userResult?.R?.toString(),
+      i: userResult?.I?.toString(),
+      a: userResult?.A?.toString(),
+      s: userResult?.S?.toString(),
+      e: userResult?.E?.toString(),
+      c: userResult?.C?.toString(),
+    } as { [key: string]: string | undefined | null };
+    
+    userResult?.UserProfessions?.forEach((profession, index) => {
+      const valueKey = `${index + 1}`;
+      const profKey = `prof${valueKey}`;
+    
+      json[profKey] = profession.name_ru; 
+      json[valueKey] = profession.percent.toString(); 
+    });
+    
+
+     const response = await fetch(
+      "https://script.google.com/macros/s/AKfycbzJXxoA9ZzgcgCxn9b5Ay73iFT2SqkojKBuxc9OvjkGjVN1uLXxkojzO0m3GDZS5u8K/exec",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(json),
+      }
+    );
 
     return;
   } catch (err) {
